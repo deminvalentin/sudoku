7b870d4d7268037342a2f885ba2b4f70fefe6ffe
deminvalentin <valentine_d@mail.ru>
2015-07-25 00:42:09 +0500

    Add game logic

--- app/Main.hs
+++ app/Main.hs
@@ -1,20 +1,98 @@
-module Main where
+{-# LANGUAGE TupleSections #-}
+module Main (main) where
 
+import           Control.Applicative ((<$>),(<*>))
+import           Control.Monad.State
+import           Data.List ((\\))
 import qualified Data.Map as M
+import           Data.Maybe (catMaybes, fromMaybe)
 
--- | Small (Row, Column) coordinate
-data SRow = SA | SB | SC deriving (Show)
-data SCol = S1 | S2 | S3 deriving (Show)
+data Digit = D1 | D2 | D3 | D4 | D5 | D6 | D7 | D8 | D9 deriving (Eq,Ord,Enum,Bounded)
+instance Show Digit where
+  show x = show $ fromEnum x
 
--- | Large (Row, Column) coordinate
-data LRow = LA | LB | LC deriving (Show)
-data LCol = L1 | L2 | L3 deriving (Show)
+-- Small coordinates
+data SRow = S1 | S2 | S3 deriving (Show, Enum, Ord, Eq, Bounded)
+data SCol = SA | SB | SC deriving (Show, Enum, Ord, Eq, Bounded)
 
--- | Digits
-data Dig = D1 | D2 | D3 | D4 | D5 | D6 | D7 | D8 |D9 deriving (Show)
+-- Large coordinates
+data LRow = L1 | L2 | L3 deriving (Show, Enum, Ord, Eq, Bounded)
+data LCol = LA | LB | LC deriving (Show, Enum, Ord, Eq, Bounded)
 
-type SmallField = M.Map (SRow, SCol) Dig
-type LargeField = M.Map (LRow, LCol) SmallField
+type SmallField = M.Map (SRow, SCol) Digit
+
+type BigField = M.Map (LRow, LCol) SmallField
+
+data Constraint = Row SmallField | Col SmallField
+
+showMbD :: Maybe Digit -> String
+showMbD Nothing = "."
+showMbD (Just d) = show $ succ $ fromEnum d
+
+allVals :: (Enum a, Bounded a) => [a]
+allVals = [minBound .. maxBound]
+
+showSmall :: SmallField -> String
+showSmall m = unlines $ map (\r -> concatMap (\c -> showMbD $ M.lookup (r,c) m) allVals) allVals
+
+emptyField = M.empty
+
+select :: [a] -> [(a,[a])]
+select [] = []
+select (x:xs) = (x,xs) : [(y,x:ys) | (y,ys) <- select xs]
+
+smallPos :: [(SRow,SCol)]
+smallPos = [(x,y) | x <- allVals, y <- allVals]
+
+getRowsD :: SRow -> SmallField -> [Digit]
+getRowsD r sf = catMaybes $ map (\c -> M.lookup (r,c) sf) allVals
+
+getColsD :: SCol -> SmallField -> [Digit]
+getColsD c sf = catMaybes $ map (\r -> M.lookup (r,c) sf) allVals
+
+guardSmallField :: Digit -> (SRow, SCol) -> Constraint -> Bool
+guardSmallField d (r,_) (Row fld) = not $ elem d $ getRowsD r fld
+guardSmallField d (_,c) (Col fld) = not $ elem d $ getColsD c fld
+
+genWithConstraint :: SmallField -> [Constraint] -> [SmallField]
+genWithConstraint init cs = flip evalStateT (allVals \\ M.elems init) $ (M.union init . M.unions) <$> forM (smallPos \\ M.keys init) (
+    \c -> do d <- StateT select
+             guard $ and $ map (guardSmallField d c) cs
+             return $ M.singleton c d
+    )
+
+mkConstraint :: BigField -> (LRow, LCol) -> [Constraint]
+mkConstraint bf (r,c) = let co = catMaybes [Col <$> M.lookup (t,c) bf | t <- allVals \\ [r]]
+                            ro = catMaybes [Row <$> M.lookup (r,t) bf | t <- allVals \\ [c]]
+                        in co ++ ro
+
+updBig :: BigField -> (LRow, LCol) -> [BigField]
+updBig init x = let cs  = mkConstraint init x
+                    fld = fromMaybe emptyField $ M.lookup x init
+                    fs = genWithConstraint fld cs
+                in map (\a -> M.union (M.singleton x a) init) fs
+
+solve :: BigField -> [BigField]
+solve init = foldM updBig init $ [(r,c) | r <- allVals, c <- allVals]
+
+readMbD :: Char -> Maybe Digit
+readMbD '1' = Just D1
+readMbD '2' = Just D2
+readMbD '3' = Just D3
+readMbD '4' = Just D4
+readMbD '5' = Just D5
+readMbD '6' = Just D6
+readMbD '7' = Just D7
+readMbD '8' = Just D8
+readMbD '9' = Just D9
+readMbD  _  = Nothing
+
+readBig :: String -> BigField
+readBig s = let mbDigs = map readMbD $ concat $ lines s
+                coords = [ ((rb,cb),(rm,cm)) | rb <- allVals, rm <- allVals, cb <- allVals, cm <- allVals]
+                emptyBig = M.unions [M.singleton (r,c) emptyField | r <- allVals, c <- allVals ]
+                listDig = catMaybes $ zipWith (\d (xb,xm) -> ((xb,) . M.singleton xm) <$> d) mbDigs coords
+            in foldl (\b (xb,sf) -> M.adjust (M.union sf) xb b) emptyBig listDig
 
 main :: IO ()
 main = putStrLn "Not yet working..."
